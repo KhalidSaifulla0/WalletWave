faa45cb5608bd88e07d7208e8bce9dd30771bc21
KhalidSaifulla0 <cavesontop0@gmail.com>
2024-08-07 22:31:48 +0530

    added appbar component

---
+++ packages/ui/src/appbar.tsx
@@ -0,0 +1,24 @@
+import { Button } from "./button";
+
+interface AppbarProps {
+  user?: {
+    name?: string | null;
+  };
+  onSignin: any;
+  onSignout: any;
+}
+
+export const Appbar = ({ user, onSignin, onSignout }: AppbarProps) => {
+  return (
+    <div className="flex justify-between border-b px-4 border-slate-300">
+      <div className="text-lg flex flex-col justify-center text-2xl font-bold">
+        WalletWave
+      </div>
+      <div className="flex flex-col justify-center pt-2">
+        <Button onClick={user ? onSignout : onSignin}>
+          {user ? "Logout" : "Login"}
+        </Button>
+      </div>
+    </div>
+  );
+};
