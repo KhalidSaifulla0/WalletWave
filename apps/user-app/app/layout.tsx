6906450fd9bc7db6d9f7623daa5c416cbe697ba4
KhalidSaifulla0 <cavesontop0@gmail.com>
2024-08-07 21:53:39 +0530

    added recoil to user-app

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import { Inter } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-});
+import { Providers } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <Providers>
+        <body className={inter.className}>{children}</body>
+      </Providers>
     </html>
   );
 }
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,3 @@
-
-
 export default function Home() {
-  return (
-    <div>
-      User App
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
