079c81d4b020723334359e384509573d87350bb7
KhalidSaifulla0 <cavesontop0@gmail.com>
2024-08-07 21:57:04 +0530

    updated db schema

---
+++ packages/db/prisma/migrations/20240807162549_updated_schema/migration.sql
@@ -0,0 +1,73 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[number]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `number` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "AuthType" AS ENUM ('Google', 'Github');
+
+-- CreateEnum
+CREATE TYPE "OnRampStatus" AS ENUM ('Success', 'Failure', 'Processing');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "number" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT NOT NULL,
+ALTER COLUMN "email" DROP NOT NULL;
+
+-- CreateTable
+CREATE TABLE "p2pTransfer" (
+    "id" SERIAL NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL,
+    "fromUserId" INTEGER NOT NULL,
+    "toUserId" INTEGER NOT NULL,
+
+    CONSTRAINT "p2pTransfer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OnRampTransaction" (
+    "id" SERIAL NOT NULL,
+    "status" "OnRampStatus" NOT NULL,
+    "token" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "OnRampTransaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Balance" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "locked" INTEGER NOT NULL,
+
+    CONSTRAINT "Balance_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "OnRampTransaction_token_key" ON "OnRampTransaction"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Balance_userId_key" ON "Balance"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_number_key" ON "User"("number");
+
+-- AddForeignKey
+ALTER TABLE "p2pTransfer" ADD CONSTRAINT "p2pTransfer_fromUserId_fkey" FOREIGN KEY ("fromUserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "p2pTransfer" ADD CONSTRAINT "p2pTransfer_toUserId_fkey" FOREIGN KEY ("toUserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OnRampTransaction" ADD CONSTRAINT "OnRampTransaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Balance" ADD CONSTRAINT "Balance_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
