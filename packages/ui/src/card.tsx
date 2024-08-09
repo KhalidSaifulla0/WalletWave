fe6000f641a61a39438396b5ff31c0f242f720b4
KhalidSaifulla0 <cavesontop0@gmail.com>
2024-08-09 23:35:03 +0530

    add new styles to card

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -8,7 +8,7 @@ export function Card({
   children?: React.ReactNode;
 }): JSX.Element {
   return (
-    <div className="border p-4">
+    <div className="border p-6 bg-white rounded-xl bg-[#ededed]">
       <h1 className="text-xl border-b pb-2">{title}</h1>
       <p>{children}</p>
     </div>
